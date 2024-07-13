e6e617e03e82b3b55a9544da154b95cf73fd3052
Cody Keener <codymkeener@gmail.com>
2024-07-13 10:58:12 -0500

    Update styling on navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -14,21 +14,35 @@ export default function NavBar() {
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <Navbar.Brand>Water Management</Navbar.Brand>
         </Link>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+          <Nav style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'space-between',
+          }}
+          >
             {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Link passHref href="/post/edit/new">
-              <Nav.Link>Add Post</Nav.Link>
-            </Link>
-            <Button variant="danger" onClick={signOut}>
-              Sign Out
-            </Button>
+            <div style={{
+              display: 'flex',
+            }}
+            >
+              <Link passHref href="/">
+                <Nav.Link>Home</Nav.Link>
+              </Link>
+              <Link passHref href="/post/edit/new">
+                <Nav.Link>Add Post</Nav.Link>
+              </Link>
+            </div>
+            <div style={{
+              display: 'flex',
+              marginLeft: 'auto',
+            }}
+            >
+              <Button variant="danger" onClick={signOut}>Sign Out</Button>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
